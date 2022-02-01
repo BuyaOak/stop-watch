f94b6453453586db268fd765d370b33c05bf58b0
ayub bey <ayub.bey@gmail.com>
2022-02-01 14:59:07 +0200

    basic styling finished

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // will add logic soon
+const Startbutton = document.querySelector(".start-button")
+const Stopbutton = document.querySelector(".stop-button")
+const Resetbutton = document.querySelector(".reset-button")
+const seconds = "00";
+const milliseconds = "00";
+
+
+
